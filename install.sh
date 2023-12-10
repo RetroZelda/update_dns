ef4ad35aa2ff23f79d7744ab06aa872f55ea9d4c
RetroZelda <efolckemer@retrozelda.com>
2023-12-10 18:47:34 -0500

    run on a timer instead of only once at boot/reconnect

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 cp ./on-network-reconnect.service /etc/systemd/system/on-network-reconnect.service
+cp ./on-network-reconnect.timer /etc/systemd/system/on-network-reconnect.timer
 chown root:root /etc/systemd/system/on-network-reconnect.service
+chown root:root /etc/systemd/system/on-network-reconnect.timer
 systemctl daemon-reload
-systemctl enable on-network-reconnect.service
+systemctl start on-network-reconnect.timer
+systemctl enable on-network-reconnect.timer
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 systemctl disable on-network-reconnect.service
+systemctl disable on-network-reconnect.timer
 rm /etc/systemd/system/on-network-reconnect.service
+rm /etc/systemd/system/on-network-reconnect.timer
 systemctl daemon-reload
